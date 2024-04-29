2321ba4020700cd0b74dbf31ed9fa4113583170c
anuragchhetri <karanchheti@gmail.com>
2024-04-29 14:56:27 +0530

    changes to check deployement issue 2

--- main.py
+++ main.py
@@ -1,21 +1,21 @@
 from fastapi import FastAPI
-# from app.routers import content_generation_service
-# from fastapi.middleware.cors import CORSMiddleware
+from app.routers import content_generation_service
+from fastapi.middleware.cors import CORSMiddleware
 
 
 app = FastAPI()
 
-# origins = ["*"]
+origins = ["*"]
 
-# app.add_middleware(
-#     CORSMiddleware,
-#     allow_origins=origins,
-#     allow_credentials=True,
-#     allow_methods=["*"],
-#     allow_headers=["*"],
-# )
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
-# app.include_router(content_generation_service.router, prefix="/textGen")
+app.include_router(content_generation_service.router, prefix="/textGen")
 
 
 
